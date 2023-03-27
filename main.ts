9d9d8f5d4d16fb5c54d26806d28ed6cbab18dea0
anchenayres <91013859+anchenayres@users.noreply.github.com>
2023-03-27 16:03:12 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+
+//TODO: Import our Typegoose and Mongoose to connect to db
 dotenv.config();
 
 const app: Express = express();
@@ -11,10 +13,15 @@ app.use(cors()); //avoid cors error
 app.use(express.json()); //to get our params from body
 
 //declare variables
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 //endpoints
 app.get("/", (req, res) => {
     res.send("Working Server");
+})
+
+//listener
+app.listen(port, () => {
+    console.log("[server: Server running at http://localhost:" + port);
 })
 
