a06ad954699d25fc095bc19ee02c6bcd0042d962
anchenayres <21100284@virtualwindow.co.za>
2023-04-12 08:33:34 +0200

    created the user.ts file

---
+++ models/user.ts
@@ -0,0 +1,15 @@
+import { prop, getModelForClass } from "@typegoose/typegoose";
+
+class User {
+    @prop({required: true})
+    public username?: string;
+
+    @prop({required: true})
+    public password?: string;
+
+    @prop({required: true})
+    public role?: string;
+
+}
+
+export const UserModel = getModelForClass(User);
